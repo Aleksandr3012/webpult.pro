c0a46bfc79270016bbc1a907a6c8d074de6ea15f
Aleksandr3012 <flaming541@gmail.com>
2020-10-14 15:37:56 +0300

    09

--- public/js/common.js
+++ public/js/common.js
@@ -263,7 +263,7 @@ function eventHandler() {
 
 	var x = window.location.host;
 	var screenName;
-	screenName = '20.jpg';
+	screenName = '09.png';
 
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after("<div class=\"pixel-perfect\" style=\"background-image: url(screen/".concat(screenName, ");\"></div>"));
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -252,7 +252,7 @@ function eventHandler() {
 	// добавляет подложку для pixel perfect
 	var x = window.location.host;
 	let screenName;
-	screenName = '20.jpg';
+	screenName = '09.png';
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
