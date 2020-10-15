eae92dab23bc6c504aad69efa3ab9ab1429cf006
Aleksandr3012 <flaming541@gmail.com>
2020-10-15 03:22:07 +0300

    17

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -262,7 +262,7 @@ function eventHandler() {
 	// добавляет подложку для pixel perfect
 	var x = window.location.host;
 	let screenName;
-	screenName = '10.png';
+	screenName = '17.png';
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
