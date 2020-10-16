a83aa98e12b8c926e752475a5fa11e7031e32d3b
Pavel <wol1414@gmail.com>
2020-10-16 13:46:38 +0300

    edit sticky menu

--- public/js/common.js
+++ public/js/common.js
@@ -509,46 +509,49 @@ function eventHandler() {
 			stickTo: '.stickyContainer'
 		});
 	} // // Cache selectors
-	// var lastId,
-	// 	solutionsMenu = $(".solutionsMenu-js"),
-	// 	solutionsMenuHeight = 20,
-	// 	// solutionsMenuHeight = solutionsMenu.outerHeight()+15,
-	// 	// All list items
-	// 	menuItems = solutionsMenu.find(".solutionsMenu__item"),
-	// 	// Anchors corresponding to menu items
-	// 	scrollItems = menuItems.map(function () {
-	// 		var item = $($(this).attr("href"));
-	// 		if (item.length) { return item; }
-	// 	});
-	// 	menuItems.click(function (e) {
-	// 		var href = $(this).attr("href"),
-	// 			offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight - 10;
-	// 		$('html, body').stop().animate({
-	// 			scrollTop: offsetTop
-	// 		}, 1100);
-	// 		e.preventDefault();
-	// 	});
-	// 	// Bind to scroll
-	// 	$(window).scroll(function () {
-	// 		// Get container scroll position
-	// 		var fromTop = $(this).scrollTop() + topMenuHeight;
-	// 		// Get id of current scroll item
-	// 		var cur = scrollItems.map(function () {
-	// 			if ($(this).offset().top < fromTop)
-	// 				return this;
-	// 		});
-	// 		// Get the id of the current element
-	// 		cur = cur[cur.length - 1];
-	// 		var id = cur && cur.length ? cur[0].id : "";
-	// 		if (lastId !== id) {
-	// 			lastId = id;
-	// 			// Set/remove active class
-	// 			menuItems
-	// 				.removeClass("active").parent()
-	// 				.end().filter("[href='#" + id + "']").addClass("active");
-	// 		}
-	// 	});
 
+
+	var lastId,
+			solutionsMenu = $(".solutionsMenu--js"),
+			solutionsMenuHeight = 20,
+			topMenuHeight = solutionsMenu.height(),
+			// solutionsMenuHeight = solutionsMenu.outerHeight()+15,
+	// All list items
+	menuItems = solutionsMenu.find(".solutionsMenu__item"),
+			// Anchors corresponding to menu items
+	scrollItems = menuItems.map(function () {
+		var item = $($(this).attr("href"));
+
+		if (item.length) {
+			return item;
+		}
+	});
+	menuItems.click(function (e) {
+		var href = $(this).attr("href"),
+				offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight - 10;
+		$('html, body').stop().animate({
+			scrollTop: offsetTop
+		}, 1100);
+		e.preventDefault();
+	}); // Bind to scroll
+
+	$(window).scroll(function () {
+		// Get container scroll position
+		var fromTop = $(this).scrollTop() + topMenuHeight; // Get id of current scroll item
+
+		var cur = scrollItems.map(function () {
+			if ($(this).offset().top < fromTop) return this;
+		}); // Get the id of the current element
+
+		cur = cur[cur.length - 1];
+		var id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id; // Set/remove active class
+
+			menuItems.removeClass("active").parent().end().filter("[href='#" + id + "']").addClass("active");
+		}
+	});
 }
 
 ;
